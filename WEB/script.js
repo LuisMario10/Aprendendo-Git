ce6221e95dacb197e6ced4c134188db5133793e3
Luis Mario <125088392+LuisMario10@users.noreply.github.com>
2025-09-30 18:52:28 -0300

    Delete WEB/script.js

--- WEB/script.js
+++
@@ -1,7 +0,0 @@
-var divDisplay = document.querySelector("#display");
-var date = new Date;
-var day = date.getDay();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-divDisplay.innerHTML = `Day: ${day} - Time: ${hours}:${minutes}`
-alert("oi");
